b12a695ea9a70b9b2b6307592b95fc044506b5be
kwakhyun <khyun2348@naver.com>
2022-12-28 13:18:23 +0900

    Feat: navigation custom hook

---
+++ src/hooks/useRootStackParamList/index.tsx
@@ -0,0 +1,10 @@
+import {
+  ParamListBase,
+  NavigationProp,
+  useNavigation
+} from '@react-navigation/native';
+
+export function useRootStackParamList() {
+  const RootStackParamList = useNavigation<NavigationProp<ParamListBase>>();
+  return RootStackParamList;
+}
